133b60d5a2448017d7a7dd60439f1e7ba9758370
Skys-Thunder <CROUD77@outlook.jp>
2025-10-12 21:36:19 +0900

    Migrate the database to Superbase

--- index.js
+++ index.js
@@ -5,31 +5,33 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import {fileURLToPath} from "url";
 import crypto from "crypto";
-import sqlite3 from "sqlite3";
+import { createClient } from "@supabase/supabase-js";
+import rateLimit from "express-rate-limit";
+import dotenv from "dotenv";
 
+dotenv.config();
 const dirname=path.dirname(fileURLToPath(import.meta.url));
 const app=express();
-const db=new sqlite3.Database("data.sqlite3");
+const supabase=createClient("https://vmpwfhgwhzwhlauhpwzh.supabase.co",process.env.supabase_key);
 const sha256=(n)=>crypto.createHash("sha256").update(n).digest("hex");
 
-function islogin(req){
-    return new Promise(resolve=>{
-        const sessionid=req.cookies.sessionid;
-        if(!sessionid) return resolve({islogin:false});
-        db.get("select * from users where sessionid = ?",[sessionid],(err,dt)=>{
-            if(!dt) return resolve({islogin:false});
-            if((Date.now()-dt.expire)/1000>60*60*24) return resolve({islogin:false});
-            return resolve({islogin:true,profile:dt});
-        })
-    });
+async function islogin(req){
+    const sessionid=req.cookies.sessionid;
+    if(!sessionid) return {islogin:false};
+    const dt=(await supabase.from("users").select("*").eq("sessionid",sessionid).limit(1)).data[0];
+    if(!dt) return {islogin:false};
+    if((Date.now()-new Date(dt.expire))/1000>60*60*24) return {islogin:false};
+    return {islogin:true,profile:dt};
 }
 
 app.set("view engine","ejs");
 app.use(express.static(path.join(dirname,"public")));
 app.use(express.json());
+app.use(rateLimit({windowMs:60*1000,limit:60*5,message:"Too many requests!",standardHeaders:true}));
 app.use(cookieParser());
 app.use(async(req,res,next)=>{
     console.log(`${new Date().toLocaleString()} page:${req.url}`);
+    // ipアクセス制限 console.log(req.ip);
     const lgdt=await islogin(req);
     res.locals.islogin=lgdt.islogin;
     if(lgdt.islogin){
@@ -41,22 +43,19 @@ app.use(async(req,res,next)=>{
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
-app.post("/api/login",(req,res)=>{
+app.post("/api/login",async(req,res)=>{
     const {username,password}=req.body;
-    db.serialize(()=>{
-        db.get("select * from users where username = ?",[username],(err,dt)=>{
-            console.log(`${username} Login trying`);
-            if(!dt) return res.status(403).json({success:false});
-            if(dt.password==sha256(password)){
-                const sessionid=crypto.randomBytes(16).toString("hex");
-                res.cookie("sessionid",sessionid,{httpOnly:true,secure:true,maxAge:24*60*60*1000});
-                res.status(200).json({success:true,role:dt.role});
-                db.run("update users set sessionid = ?, expire = ? where username = ?",[sessionid,Date.now(),username]);
-                console.log(`${dt.username} Login succeed!`);
-            }
-            else res.status(403).json({sucess:false});
-        })
-    });
+    console.log(`${username} Login trying`);
+    const dt=(await supabase.from("users").select("*").eq("username",username).limit(1)).data[0];
+    if(!dt) return res.status(403).json({success:false});
+    if(dt.password==sha256(password)){
+        const sessionid=crypto.randomBytes(16).toString("hex");
+        await supabase.from("users").update({sessionid,expire:new Date().toISOString()}).eq("username",username);
+        res.cookie("sessionid",sessionid,{httpOnly:true,secure:true,maxAge:24*60*60*1000});
+        res.status(200).json({success:true,role:dt.role});
+        console.log(`${username} Login succeeded!`);
+    }
+    else res.status(403).json({success:false});
 });
 app.get("/login",(req,res)=>{
     // ログイン完了後、元のページに戻す実装をする！
